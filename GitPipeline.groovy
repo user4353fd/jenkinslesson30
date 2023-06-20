b4d12dfa92a7dc4eac75e3241fc04e0980281a71
user4353fd <117071270+user4353fd@users.noreply.github.com>
2023-06-20 04:55:54 +0300

    Add files via upload

---
+++ GitPipeline.groovy
@@ -0,0 +1,76 @@
+pipeline {
+  agent {
+    label "master"
+  }
+  
+  parameters {
+    string(
+      name: "branch_name",
+      description: "Branch name to checkout",
+      defaultValue: "master"
+    )
+  }
+
+  stages {
+    stage("Checkout") {
+      steps {
+        git(
+            branch: params.branch_name,
+            credentialsId: 'ssh-key',
+            url: "git@github.com:user4353fd/user4353fd-react-redux-realworld-example-app.git",
+                       
+        )
+      }
+    }
+
+    stage("Build docker image") {
+      steps {
+        script {
+          docker.build "react-app:test"
+        }
+      }
+    }
+  }
+}
+
+// pipeline {
+//   agent {
+//     label "dind"
+//   }
+  
+//   parameters {
+//     string(
+//       name: "branch_name",
+//       description: "Branch name to checkout",
+//       defaultValue: "master"
+//     )
+//   }
+
+//   stages {
+//     stage("Checkout") {
+//       steps {
+//         sshagent(credentials: ['ssh-key']) {
+//           sh 'git clone git@github.com:user4353fd/user4353fd-react-redux-realworld-example-app.git'
+//         }
+//       }
+//     }
+
+//     stage("Build docker image") {
+//       steps {
+//         script {
+//           docker.build "react-app:test"
+//         }
+//       }
+//     }
+  
+
+
+//     stage("Build docker image") {
+//       steps {
+//         script {
+//           docker.build "react-app:test"
+//         }
+//       }
+//     }
+//   }
+// }
